189677eb7d86b3157248a56734542cf2231f7f4e
ASK-STEM部 <stem.ask.111@gmail.com>
2025-10-24 05:37:26 +0000

    schemeがpublicじゃなくてmemberだかｒ

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -8,6 +8,9 @@ export const createClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      db: {
+        schema: 'member',
+      },
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
