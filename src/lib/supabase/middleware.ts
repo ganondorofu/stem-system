ec4e4adc0334b244d2fd1dae4ac1cd2bd7c1e1a3
ganondorofu <ganondorofu123@gmail.com>
2025-10-26 04:10:49 +0000

    https://test-app.yn3143.xyz/loginに戻ってくる

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -12,9 +12,6 @@ export async function updateSession(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      db: {
-        schema: 'member',
-      },
       cookies: {
         get(name: string) {
           return request.cookies.get(name)?.value
