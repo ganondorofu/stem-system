8eea7ef89807ee405ba32a21c72da48417832a2d
ganondorofu <167788682+ganondorofu@users.noreply.github.com>
2025-11-08 01:42:02 +0000

    ページでメインコンテンツ部分をスクロールするとサイドバーまでスクロールされてしまう

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -30,11 +30,11 @@ export default async function DashboardLayout({
   } : null;
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full overflow-hidden md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <DashboardSidebar user={fullProfile} />
       <div className="flex flex-col">
         <DashboardHeader user={fullProfile} />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/20">
+        <main className="flex flex-1 flex-col gap-4 overflow-auto p-4 lg:gap-6 lg:p-6 bg-muted/20">
           {children}
         </main>
       </div>
